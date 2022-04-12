d4ef1286767445a33cbf822655add4f0a025df6c
EdgarGarciaMar <edgargarcia092010@gmail.com>
2022-04-11 19:35:47 -0500

    Update, ya se envian objetos

--- chat/src/main/java/conexion/enviarMsj.java
+++ chat/src/main/java/conexion/enviarMsj.java
@@ -4,11 +4,15 @@
  */
 package conexion;
 
+import datos.Cliente;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
 import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -18,24 +22,52 @@ import java.util.logging.Logger;
  */
 public class enviarMsj {
 
-    public static void main(String[] args) throws IOException {
-        MulticastSocket enviador = new MulticastSocket();
+    byte buffer[] = new byte[6500];
+    MulticastSocket enviador;
+    DatagramPacket dgp;
 
-// El dato que queramos enviar en el mensaje, como array de bytes.
-        byte[] dato = new byte[]{1, 2, 3, 4};
-
-// Usamos la direccion Multicast 230.0.0.1, por poner alguna dentro del rango
-// y el puerto 55557, uno cualquiera que esté libre.
-        DatagramPacket dgp;
+    public enviarMsj() {
         try {
-            dgp = new DatagramPacket(dato, dato.length, InetAddress.getByName("230.0.0.1"), 55557);
-            // Envío
-            enviador.send(dgp);
-            System.out.println("Se envio el dato:"+dato+", su tam: "+dato.length);
-        } catch (UnknownHostException ex) {
-            Logger.getLogger(recivirMsj.class.getName()).log(Level.SEVERE, null, ex);
+            enviador = new MulticastSocket();
+        } catch (IOException ex) {
+            Logger.getLogger(enviarMsj.class.getName()).log(Level.SEVERE, null, ex);
         }
 
     }
-}
 
+    public void cerrarCliente() {
+        enviador.close();
+    }
+
+    public void enviar() throws IOException {
+        Cliente c;
+        c = new Cliente("Hola, como estas?", "Edgar", "Todos");
+
+        ByteArrayOutputStream bs = new ByteArrayOutputStream();
+        ObjectOutputStream os = new ObjectOutputStream(bs);
+        os.writeObject(c);  // this es de tipo DatoUdp
+        os.close();
+
+        buffer = bs.toByteArray();
+        // Usamos la direccion Multicast 230.0.0.1, por poner alguna dentro del rango
+        // y el puerto 55557, uno cualquiera que esté libre.
+
+        try {
+            dgp = new DatagramPacket(buffer, buffer.length, InetAddress.getByName("230.0.0.1"), 4000);//55557 puerto privado
+            // Envío
+            enviador.send(dgp);
+            System.out.println(c.getUsuarioOrigen() + ":" + c.getMensaje());
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(recibirMsj.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        enviarMsj c;
+
+        c = new enviarMsj();
+
+        c.enviar();
+        c.cerrarCliente();
+    }
+}
